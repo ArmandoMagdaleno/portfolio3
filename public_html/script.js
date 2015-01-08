704709fc85dd50803b766b862d4bcd9d46da510f
Armando Magdaleno <magdalenoarmando90@gmail.com>
2015-01-08 11:26:54 -0800

    2nd

--- public_html/script.js
+++ public_html/script.js
@@ -6,7 +6,11 @@
 
 $("document").ready(function(){
     $(".lbj").css("background-color", "gold");
-    
+    $("body").css("background-color", "black");
+    $("h1").css({"background-color": "red", "color": "white"});
+    $("p:lt(3)").css({"background-color": "maroon", "color": "white"});
+    $("p:gt(2)").css({"background-color": "white", "color": "maroon"});
+   
 });
 
 
