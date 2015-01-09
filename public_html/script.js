4b0b0b815be9947e39397310cf48009a17a9c397
Armando Magdaleno <magdalenoarmando90@gmail.com>
2015-01-09 11:27:40 -0800

    4

--- public_html/script.js
+++ public_html/script.js
@@ -4,13 +4,26 @@
  * and open the template in the editor.
  */
 
-$("document").ready(function(){
+$("document").ready(function() {
     $(".lbj").css("background-color", "gold");
     $("body").css("background-color", "black");
     $("h1").css({"background-color": "red", "color": "white"});
     $("p:lt(3)").css({"background-color": "maroon", "color": "white"});
     $("p:gt(2)").css({"background-color": "white", "color": "maroon"});
-   
+    $("h1").bind("mouseover", mouseOverMe).bind("mouseout", mouseOutMe);
+    $("h1").bind("click", mouseClick);
+
 });
 
+function mouseOverMe() {
+    $("h1").html("GREATEST OF ALL TIME");
+}
+
+function mouseOutMe() {
+    $("h1").html("LEBRON JAMES");
+}
+
+function mouseClick() {
+    $("h1").html("CLUTCH");
+}
 
